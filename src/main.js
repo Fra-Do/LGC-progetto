86234f2c919597c641f27ff1a296e0d00a773ca5
Francesca <francescadonghi03@gmail.com>
2024-11-12 10:37:59 +0100

    intro primi codice di main player e level1

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+let config = {
+    canvas_width      : 1280,
+    canvas_height     : 720,
+    canvas_id         : "game_area", // Specifica il div contenitore
+    background_color  : 0x000055,
+    debug_mode        : true, //quando consegnamo il progetto mettiamolo false
+    gravity_value     : 200
+};
+
+PP.game.create(config);
---
+++ src/player.js
@@ -0,0 +1,17 @@
+function preload_player(s) {
+
+}
+
+function create_player(s) {
+
+}
+
+function update_player(s) {
+    
+}
+
+function destroy_player(s) {
+
+}
+
+PP.scenes.add("player", preload, create, update, destroy);
