a911e148517f50ccc9f18d57f2fbd6f3b5fbc264
Martina <marpol806@gmail.com>
2024-11-18 11:45:58 +0100

    player aggiunto

--- src/player.js
+++ src/player.js
@@ -1,17 +1,29 @@
-function preload_player(s) {
+let img_player;
+let player;
+let player_speed =   200;
 
+
+function preload_player(s) {
+    img_player = PP.assets.image.load(s, "assets/images/stickman.png");
 }
 
 function create_player(s) {
-
+    img_player = PP.assets.image.add(s, "assets/images/stickman.png");
 }
 
 function update_player(s) {
-    
+    if(PP.interactive.kb.is_key_down(s, PP.key_codes.RIGHT)) {
+        PP.physics.set_velocity_x(img_player, player_speed);
+    } 
+    else if (PP.interactive.kb.is_key_down(s, PP.key_codes.LEFT)) {
+        PP.physics.set_velocity_x(img_player, -player_speed);
+    } //else {
+        //PP.physics.set_velocity_x(player, 0);
+       // next_anim = "stop";
+    //}
 }
 
 function destroy_player(s) {
 
 }
 
-PP.scenes.add("player", preload, create, update, destroy);
