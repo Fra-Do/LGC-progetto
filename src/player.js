b83b5267fc5b8596fe402d6d1c6ae49f0dab55bc
Arianna <arianna.doardo@mail.polimi.it>
2024-11-19 13:00:37 +0100

    modifiche player

--- src/player.js
+++ src/player.js
@@ -1,17 +1,32 @@
 let img_player;
 let player;
 
-let player_speed =   200;
+let player_speed =   300;
 let step_lenght = 4;
 let height = 5;
+let jump_init_speed = 300;
+let floor_height    = 615;
+
+let curr_anim = "stop";
+
+function configure_player_animations(s) {
+    // Configuro le animazioni secondo lo spritesheet
+    PP.assets.sprite.animation_add_list(player, "run", [6, 13, 20, 27, 34], 10, -1); // Lista di frame, a 10 fps, inifito
+    PP.assets.sprite.animation_add(player, "jump_up", 36, 36, 10, 0);
+    PP.assets.sprite.animation_add(player, "jump_down", 42, 45, 10, 0);
+    PP.assets.sprite.animation_add(player, "stop", 21, 21, 10, 0);
+    PP.assets.sprite.animation_play(player, "stop");
+
+}
 
 
 function preload_player(s) {
-    img_player = PP.assets.image.load(s, "assets/images/stickman.png");
+    img_player = PP.assets.image.load(s, "assets/images/spritesheet_player.png", 223, 190);
 }
 
 function create_player(s) {
     player = PP.assets.image.add(s, img_player, 120, 300, 1, 1);
+    PP.physics.add(s, player, PP.physics.type.DYNAMIC); 
 }
 
 function update_player(s) {
@@ -28,6 +43,29 @@ function update_player(s) {
 
     if (PP.interactive.kb.is_key_down(s, PP.key_codes.DOWN)) {
         player.geometry.y += height;
+    }
+
+    if(player.geometry.y>=floor_height-1 || player.is_on_platform) {
+        // Se mi trovo sul pavimento OPPURE su una piattaforma...
+
+        if(PP.interactive.kb.is_key_down(s, PP.key_codes.SPACE)) {
+            // ... e premo il tasto spazio, allo salto
+            PP.physics.set_velocity_y(player, -jump_init_speed);
+        }
+
+        // Non gestisco qui le animazioni del salto, ma piu' avanti
+    }
+
+    player.is_on_platform = false;  // Resetto il flag che viene messo a true quando il giocatore 
+                                    // si trova sulla piattaforma
+
+    // Le animazioni del salto vengono gestite in base alla velocita'
+    // verticale
+    if(PP.physics.get_velocity_y(player) < 0) {
+        next_anim = "jump_up";
+    }
+    else if(PP.physics.get_velocity_y(player) > 0) {
+        next_anim = "jump_down";
     }
 }
 
