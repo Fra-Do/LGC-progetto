6e222ec4909c0c2f0fbdfd5a9abd8af2fad28c89
Arianna <arianna.doardo@mail.polimi.it>
2024-11-19 12:32:36 +0100

    aggiunta platform 1

--- src/platform1.js
+++ src/platform1.js
@@ -1,12 +1,22 @@
-function preload_platform1 (s) {
+let img_platform
+let platform;
 
+function preload_platform1 (s) {
+    img_platform   = PP.assets.image.load(s, "assets/images/platform1.png");
 }
 
 function create_platform1 (s) {
+    if( player.geometry.x >= platform.geometry.x &&
+        player.geometry.x <= platform.geometry.x + platform.geometry.display_width) {
+            player.is_on_platform = true;
+    }
 
 }
 
 function update_platform1 (s) {
+    platform = PP.assets.image.add(s, img_platform, 400, 300, 0, 0);
+    PP.physics.add(s, platform, PP.physics.type.STATIC); 
+    PP.physics.add_collider_f(s, player, platform, collision_platform);
     
 }
 
