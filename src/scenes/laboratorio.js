38bf5692b4972b8e1d0314c1e086b036b2abcd29
Arianna <arianna.doardo@mail.polimi.it>
2024-11-22 18:42:07 +0100

    modifiche main, lab e platform

--- src/scenes/laboratorio.js
+++ src/scenes/laboratorio.js
@@ -3,14 +3,14 @@ let floor;
 let background;
 
 function preload(s) {
-    img_background = PP.assets.image.load(s, "assets/images/sfondo.png");
+    img_background = PP.assets.image.load(s, "assets/images/prova_mappa.png");
     preload_player(s);
     preload_platform1 (s)
     
 }
 
 function create(s) {
-    background = PP.assets.image.add (s, img_background, 0, 0, 0, 0);
+    background = PP.assets.image.add(s, img_background, 0, 0, 0, 0);
    
     create_player(s);
     create_platform1 (s)
