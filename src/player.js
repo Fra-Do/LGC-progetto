7a10c81782953d94b0f1315fdfac3746b68b95c6
Arianna <arianna.doardo@mail.polimi.it>
2024-11-26 12:52:50 +0100

    correzione fisica

--- src/player.js
+++ src/player.js
@@ -19,11 +19,11 @@ function create_player(s) {
 function update_player(s) {
     if(PP.interactive.kb.is_key_down(s, PP.key_codes.RIGHT)) {
         PP.physics.set_velocity_x(player, player_speed);
-        player.geometry.x += step_lenght;
+        //player.geometry.x += step_lenght;
     } 
     else if (PP.interactive.kb.is_key_down(s, PP.key_codes.LEFT)) {
         PP.physics.set_velocity_x(player, -player_speed);
-        player.geometry.x -= step_lenght;  
+        //player.geometry.x -= step_lenght;  
     } else {
         // Se non e' premuto alcun tasto...
         PP.physics.set_velocity_x(player, 0);
@@ -31,12 +31,13 @@ function update_player(s) {
     }
 
     if (PP.interactive.kb.is_key_down(s, PP.key_codes.UP)) {
-        player.geometry.y -= height;
+       // player.geometry.y -= height;
+       PP.physics.set_velocity_y(player, -player_speed);
         
     }
 
     if (PP.interactive.kb.is_key_down(s, PP.key_codes.DOWN)) {
-        player.geometry.y += height;
+        //player.geometry.y += height;
     }
 }
 
