187c29a8b336bd9d7a6969834f58669dbde47322
Martina <marpol806@gmail.com>
2024-11-26 12:03:50 +0100

    livello 2

---
+++ src/scenes/livello_2.js
@@ -0,0 +1,28 @@
+let img_background;
+let floor;
+let background;
+
+function preload(s) {
+    img_background = PP.assets.image.load(s, "assets/images/livello_2.png");
+    preload_player(s);
+
+}
+
+function create(s) {
+    background = PP.assets.image.add(s, img_background, 0, -450, 0, 0);
+    create_player(s);
+    create_structure(s);
+
+    PP.camera.set_follow_offset(s, 600, 400)
+    PP.camera.start_follow(s, player, 50, 120);
+}
+
+function update(s) {
+    update_player(s);
+}
+
+function destroy(s) {
+    destroy_player(s);
+}
+
+PP.scenes.add("laboratorio", preload, create, update, destroy);
