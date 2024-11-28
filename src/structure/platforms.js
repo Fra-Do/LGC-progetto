433d0080ac97c2dcdf70f4734d4bd770242b903d
Arianna <arianna.doardo@mail.polimi.it>
2024-11-29 00:51:17 +0100

    tentativo piattaforma diagonale

--- src/structure/platforms.js
+++ src/structure/platforms.js
@@ -1,15 +1,9 @@
 let img_platforms;
 let platforms;
 let pl_1_speed = 250;
-<<<<<<< HEAD
-let pl_3_speed = 250;
-
-=======
 let pl_2_speed = 150;
 let pl_3_speed = 250;
 
-
->>>>>>> 50ac91b5ca1501d01b2497c9baaaa7352d993a01
 
 function preload_platforms(s) {
 
@@ -58,6 +52,15 @@ function create_platforms(s) {
     PP.physics.set_velocity_y(pl_2, pl_2_speed);
 
 
+    //Qua ho provato a dare una soluzione invano
+    //pl_2 = PP.shapes.rectangle_add(s, 3100, 2080, 235, 50, "0xFF0000", 0);
+    //PP.physics.add(s, pl_2_diagonal, PP.physics.type.DYNAMIC); 
+    //PP.physics.set_immovable(pl_2_diagonal, true);
+    //PP.physics.set_allow_gravity(pl_2_diagonal, false);    
+    //PP.physics.add_collider_f(s, player, pl_2_diagonal, collision_platform);
+    //PP.physics.set_velocity_y(pl_2_diagonal, -pl_2_speed);
+
+
     //piattaforma gruppo 3
     pl_3 = PP.shapes.rectangle_add(s, 3700, 2130, 410, 65, "0xFF0000", 0);
     PP.physics.add(s, pl_3, PP.physics.type.DYNAMIC); 
@@ -77,13 +80,7 @@ function create_platforms(s) {
     // PP.physics.add_collider_f(s, player, platform_2, collision_platform);
     // PP.physics.set_velocity_x(platform_2, 100);
 
-    //piattaforma 3
-    pl_3 = PP.shapes.rectangle_add(s, 3473, 2100, 409, 65, "0xFF0000", 0);
-    PP.physics.add(s, pl_3, PP.physics.type.DYNAMIC); 
-    PP.physics.set_immovable(pl_3, true);
-    PP.physics.set_allow_gravity(pl_3, false);    
-    PP.physics.add_collider_f(s, player, pl_3, collision_platform);
-    PP.physics.set_velocity_x(pl_3, pl_1_speed);
+    
 
 }
 
@@ -113,13 +110,6 @@ function update_platforms(s) {
         PP.physics.set_velocity_y(pl_1_3, pl_1_speed);
     }
 
-    //3 !!DA RIVEDERE!!
-    if(pl_3.geometry.y >= 1600) {
-        PP.physics.set_velocity_y(pl_1_3, -pl_1_speed);
-    }
-    else if(pl_3.geometry.y <= 5000) {
-        PP.physics.set_velocity_y(pl_1_3, pl_1_speed);
-    }
     
 
     //piattaforme gruppo 2      fa cose strane non sono riuscitta a farlo andare in diagonale domani riprovo
@@ -136,6 +126,17 @@ function update_platforms(s) {
     else if(pl_2.geometry.y <= 1900) {
         PP.physics.set_velocity_y(pl_2, pl_2_speed);
     }
+
+    //if (pl_2_diagonal.geometry.x <= 3250 || pl_2_diagonal.geometry.y <= 2800) {
+        // Inverti la velocità per il movimento diagonale verso l'alto a sinistra
+        //PP.physics.set_velocity_x(pl_diagonal, pl_2_speed);  // Cambia direzione orizzontale verso destra
+        //PP.physics.set_velocity_y(pl_diagonal, pl_2_speed);  // Cambia direzione verticale verso il basso
+    //}
+    //else if (pl_diagonal.geometry.x >= 2080 || pl_diagonal.geometry.y >= 1900) {
+        // Inverti la velocità per il movimento diagonale verso il basso a destra
+        //PP.physics.set_velocity_x(pl_diagonal, -pl_2_speed);  // Cambia direzione orizzontale verso sinistra
+        //PP.physics.set_velocity_y(pl_diagonal, -pl_2_speed);  // Cambia direzione verticale verso l'alto
+    //}
 
 
     //piattaforme gruppo 3
