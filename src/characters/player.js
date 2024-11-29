746e73b3f77138673e6490800194d23d21c039d9
Arianna <arianna.doardo@mail.polimi.it>
2024-11-29 22:32:42 +0100

    sprite fallimentare

--- src/characters/player.js
+++ src/characters/player.js
@@ -7,62 +7,49 @@ let height       = 5;
 
 let curr_anim = "stop";
 
-
-function configure_player_animations(s, player) {   
-    PP.assets.sprite.animation_add_list(player, "run", [1, 2, 3, 4, 5], 10, -1);
-    PP.assets.sprite.animation_add(player, "stop", 0, 0, 10, 0);
-    PP.assets.sprite.animation_play(player, "stop");
+function configure_player_animations(s, player) {
+    PP.assets.sprite.animation_add_list(player, "run", [1, 2, 3, 4, 5, 6], 10, -1);  // 6 frame per l'animazione "run"
+    PP.assets.sprite.animation_add(player, "stop", 0, 0, 10, 0);  // frame 0 per l'animazione "stop"
+    PP.assets.sprite.animation_play(player, "stop");  // avvia l'animazione "stop" di default
 }
 
 function preload_player(s) {
-    img_player = PP.assets.sprite.load_spritesheet(s, "assets/images/sprite_ragazza.png", 94, 137);
-    // img_player = PP.assets.image.load(s, "assets/images/protagonista.png");
-
+    // 6 frame disposti in una sola riga
+    img_player = PP.assets.sprite.load_spritesheet(s, "assets/images/spriteragazza.png", 94, 137, 6, 1);
 }
-
 
 function create_player(s) {
     player = PP.assets.sprite.add(s, img_player, 400, 300, 0.5, 1);
-    //player = PP.assets.image.add(s, img_player, 400, 300, 1, 1);
-    PP.physics.add(s, player, PP.physics.type.DYNAMIC); 
-    
+    PP.physics.add(s, player, PP.physics.type.DYNAMIC);  
 }
-
-
 
 function update_player(s) {
     let next_anim = curr_anim;
     
     if(PP.interactive.kb.is_key_down(s, PP.key_codes.RIGHT)) {
         PP.physics.set_velocity_x(player, player_speed);
-        next_anim = "run";
-        //player.geometry.x += step_lenght;
+        next_anim = "run";  // quando si preme freccia destra, l'animazione è "run"
     } 
     else if (PP.interactive.kb.is_key_down(s, PP.key_codes.LEFT)) {
         PP.physics.set_velocity_x(player, -player_speed);
-        next_anim = "run";
-        //player.geometry.x -= step_lenght;  
+        next_anim = "run";  // quando si preme freccia sinistra, l'animazione è "run"
     } else {
-        // Se non e' premuto alcun tasto...
-        PP.physics.set_velocity_x(player, 0);
+        PP.physics.set_velocity_x(player, 0);  // se nessun tasto è premuto, il player si ferma
         next_anim = "stop";
     }
 
+    // Aggiorniamo la direzione dell'animazione (esempio per il salto)
     if (PP.interactive.kb.is_key_down(s, PP.key_codes.UP)) {
-       // player.geometry.y -= height;
-       PP.physics.set_velocity_y(player, -player_speed);
-        
+        PP.physics.set_velocity_y(player, -player_speed);
     }
 
-    if (PP.interactive.kb.is_key_down(s, PP.key_codes.DOWN)) {
-        //player.geometry.y += height;
-    }
-
+    // Se l'animazione è cambiata, la aggiorniamo
     if (next_anim != curr_anim) {
         PP.assets.sprite.animation_play(player, next_anim);
         curr_anim = next_anim;
     }
 
+    // Flip l'immagine quando si cambia direzione
     if (PP.physics.get_velocity_x(player) < 0) {
         player.geometry.flip_x = true;
     } 
@@ -71,7 +58,6 @@ function update_player(s) {
     }
 }
 
-
 function destroy_player(s) {
+    
 }
-
