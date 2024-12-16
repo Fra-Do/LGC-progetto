80b61c2adb0b83ad6018b4526e52d53257ca7991
Martina <marpol806@gmail.com>
2024-12-16 17:57:08 +0100

    prendere kit + messaggio che ti appare di istruzione per i kit

--- src/characters/player.js
+++ src/characters/player.js
@@ -60,7 +60,7 @@ function preload_player(s) {
 function create_player(s) {
     player = PP.assets.sprite.add(s, img_player, 400, 300, 0.5, 1);
 
-    //player = PP.assets.sprite.add(s, img_player, 7600, 800, 0.5, 1);
+    //player = PP.assets.sprite.add(s, img_player, 8090, 1430, 0.5, 1);
     PP.physics.add(s, player, PP.physics.type.DYNAMIC);  
 }
 
--- src/structure/structure.js
+++ src/structure/structure.js
@@ -287,20 +287,20 @@ function create_structure(s) {
 
     //OGGETTI
     //KIT
-    //kit 1
-    wall = PP.shapes.rectangle_add(s, 1368, 347, 35, 30, "0xFF0000", 0); 
-    PP.physics.add(s, wall, PP.physics.type.STATIC); 
-    PP.physics.add_collider(s, player, wall);
+    ///kit 1
+    //wall = PP.shapes.rectangle_add(s, 1370, 265, 70, 51, "0xFF0000", 0); 
+    //PP.physics.add(s, wall, PP.physics.type.STATIC); 
+    //PP.physics.add_collider(s, player, wall);
 
     //kit 2
-    wall = PP.shapes.rectangle_add(s, 5580, 1450, 35, 30, "0xFF0000", 0); 
-    PP.physics.add(s, wall, PP.physics.type.STATIC); 
-    PP.physics.add_collider(s, player, wall);
+    //wall = PP.shapes.rectangle_add(s, 5580, 1450, 35, 30, "0xFF0000", 0); 
+    //PP.physics.add(s, wall, PP.physics.type.STATIC); 
+    //PP.physics.add_collider(s, player, wall);
 
     //kit 3
-    wall = PP.shapes.rectangle_add(s, 8090, 1443, 35, 30, "0xFF0000", 0); 
-    PP.physics.add(s, wall, PP.physics.type.STATIC); 
-    PP.physics.add_collider(s, player, wall);
+    //wall = PP.shapes.rectangle_add(s, 8090, 1443, 35, 30, "0xFF0000", 0); 
+    //PP.physics.add(s, wall, PP.physics.type.STATIC); 
+    //PP.physics.add_collider(s, player, wall);
 
     //pergamena
     wall = PP.shapes.rectangle_add(s, 1630, 347, 35, 30, "0xFF0000", 0); 
