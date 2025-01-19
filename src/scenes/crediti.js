cb44e9db60a67e31af9221e3c1be722b65b97449
Arianna <arianna.doardo@mail.polimi.it>
2025-01-19 22:29:11 +0100

    inserimento bottone da crediti a cover

--- src/scenes/crediti.js
+++ src/scenes/crediti.js
@@ -1,14 +1,25 @@
 let img_crediti;
 let crediti;
 
+let img_back;
+let back;
+
 function preload_credits (s) {
     img_crediti = PP.assets.image.load(s, "assets/images/cover/crediti.png");
+    img_back    = PP.assets.image.load(s, "assets/images/cover/indietro.png");
 
 }
 
 function create_credits (s) {
     crediti  = PP.assets.image.add(s, img_crediti, 0, 0, 0, 0);
+    back     = PP.assets.image.add(s, img_back, 20, 660, 0, 0);
 
+    PP.interactive.mouse.add (back, "pointerdown", change_scene_cover)
+}
+
+function change_scene_cover(s) {
+    console.log("Cambio scena: Livello 1");
+    PP.scenes.start("cover");
 }
 
 function update_credits (s) {
